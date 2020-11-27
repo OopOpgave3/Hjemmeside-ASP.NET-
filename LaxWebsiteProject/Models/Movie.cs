0cb5d38c22d83e3ee5d88262f726e87aeccb2faa
simon nørregaard <dwaf@live.dk>
2020-11-27 12:34:51 +0100

    Test

---
+++ LaxWebsiteProject/Models/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LaxWebsiteProject.Models
+{
+    public class Movie
+    {
+        public int PK_MovieId { get; set; }
+
+        public string MovieTitle { get; set; }
+
+        public DateTime MovieReleaseDate { get; set; }
+
+        public string MovieDirector { get; set; }
+
+        public List<MovieCategory> MovieCategories { get; set; }
+    }
+}
---
+++ LaxWebsiteProject/Models/MovieTag.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LaxWebsiteProject.Models
+{
+    public class MovieTag
+    {
+        public int PK_MovieTagID { get; set; }
+
+        public int FK_MovieId { get; set; }
+
+        public int FK_TagID { get; set; }
+    }
+}
